cea738e14a70dc3783e6823f3513d442b9d2b5d5
Cyril DUCHENOY <c.duchenoy@geco-it.fr>
2024-11-19 16:50:42 +0100

    add function - correct wp release step

--- git-log.sh
+++ git-log.sh
@@ -42,52 +42,61 @@ sayW()
 return 0
 }
 
+generate_changelog()
+{
+    if eval git describe --tags --abbrev=0 &> /dev/null; then
+        # print commit log lines to CI_COMMIT_TAG
+        if [ -n "${CI_COMMIT_TAG}" ]; then
+            PREVIOUS_TAG="$(git describe --tags --abbrev=0 "${CI_COMMIT_TAG}^" 2> /dev/null || return 1)"
+
+            # from previous tag
+            if [ -n "${PREVIOUS_TAG}" ]; then
+                ${PLUGIN_DEBUG:-false}&& say "Previous tag: ${PREVIOUS_TAG}"
+                ${PLUGIN_DEBUG:-false}&& say "New tag: ${CI_COMMIT_TAG}"
+                say "Generate changelog from '${PREVIOUS_TAG}' to '${CI_COMMIT_TAG}'..."
+                git log "${PREVIOUS_TAG}..${CI_COMMIT_TAG}" --no-merges --pretty="- %s" | tee -a CHANGELOG.md &> /dev/null || return 1
+                echo -e "\n_**Compare**_: [${PREVIOUS_TAG}...${CI_COMMIT_TAG}](${CI_REPO_URL}/compare/${PREVIOUS_TAG}...${CI_COMMIT_TAG})" >> CHANGELOG.md
+
+            # for all commits
+            else
+                ${PLUGIN_DEBUG:-false}&& say "New tag: ${CI_COMMIT_TAG}"
+                say "Generate changelog from first commit to '${CI_COMMIT_TAG}'..."
+                git log --no-merges --pretty="- %s" | tee -a CHANGELOG.md &> /dev/null
+            fi
+
+        # print all commits log lines to last commit
+        else
+            LAST_TAG="$(git describe --tags --abbrev=0 2> /dev/null || return 1)"
+
+            # from last tag
+            if [ -n "${LAST_TAG}" ]; then
+                say "Last tag: ${LAST_TAG}"
+                git log "${LAST_TAG}"..HEAD --no-merges --pretty="- %s" | tee -a CHANGELOG.md &> /dev/null || return 1
+
+            # for all commits
+            else
+                say "Found any tag in this repository... Generate full changelog..."
+                git log --no-merges --pretty="- %s" | tee -a CHANGELOG.md &> /dev/null || return 1
+            fi
+        fi
+    else
+        # all commits log lines
+        say "Found any tag in this repository... Generate full changelog..."
+        git log --no-merges --pretty="- %s" | tee -a CHANGELOG.md &> /dev/null || return 1
+    fi
+
+return 0
+}
+
 # =========================================================================================================
 # main()
 
 # changelog headers
 echo -e "# What's Changed\n" > CHANGELOG.md
 
-if eval git describe --tags --abbrev=0 &> /dev/null; then
-    # print commit log lines to CI_COMMIT_TAG
-    if [ -n "${CI_COMMIT_TAG}" ]; then
-        PREVIOUS_TAG="$(git describe --tags --abbrev=0 "${CI_COMMIT_TAG}^")"
+generate_changelog || sayE "Git changelog generated with error! 💣"
 
-        # from previous tag
-        if [ -n "${PREVIOUS_TAG}" ]; then
-            ${PLUGIN_DEBUG:-false}&& say "Previous tag: ${PREVIOUS_TAG}"
-            ${PLUGIN_DEBUG:-false}&& say "New tag: ${CI_COMMIT_TAG}"
-            say "Generate changelog from '${PREVIOUS_TAG}' to '${CI_COMMIT_TAG}'.  ✅"
-            git log "${PREVIOUS_TAG}..${CI_COMMIT_TAG}" --no-merges --pretty="- %s" | tee -a CHANGELOG.md &> /dev/null
-            echo -e "\n_**Compare**_: [${PREVIOUS_TAG}...${CI_COMMIT_TAG}](${CI_REPO_URL}/compare/${PREVIOUS_TAG}...${CI_COMMIT_TAG})" >> CHANGELOG.md
-
-        # for all commits
-        else
-            ${PLUGIN_DEBUG:-false}&& say "New tag: ${CI_COMMIT_TAG}"
-            say "Generate changelog from first commit to '${CI_COMMIT_TAG}'.  ✅"
-            git log --no-merges --pretty="- %s" | tee -a CHANGELOG.md &> /dev/null
-        fi
-
-    # print all commits log lines to last commit
-    else
-        LAST_TAG="$(git describe --tags --abbrev=0)"
-
-        # from last tag
-        if [ -n "${LAST_TAG}" ]; then
-            say "Last tag: ${LAST_TAG}"
-            git log "${LAST_TAG}"..HEAD --no-merges --pretty="- %s" | tee -a CHANGELOG.md &> /dev/null
-
-        # for all commits
-        else
-            say "Found any tag in this repository... Generate full changelog.  ✅"
-            git log --no-merges --pretty="- %s" | tee -a CHANGELOG.md &> /dev/null
-        fi
-    fi
-else
-    # all commits log lines
-    say "Found any tag in this repository... Generate full changelog.  ✅"
-    git log --no-merges --pretty="- %s" | tee -a CHANGELOG.md &> /dev/null
-fi
+say "Git changelog successfully generated. ✅"
 
 if ${PLUGIN_DEBUG:-false}; then
     sayW "Debug mode enable... Generated changelog:"
